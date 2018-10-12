295e8702902f1a0b4573550fcd7d6a0f3fb61c47
jaspers0324 <32628620+jaspers0324@users.noreply.github.com>
2018-10-12 16:37:36 +0800

    Add files via upload

--- examples/Jasper_SHT31/Jasper_SHT31.ino
+++ examples/Jasper_SHT31/Jasper_SHT31.ino
@@ -12,10 +12,10 @@ Jasper_SHT31 sht31 = Jasper_SHT31();
 void setup() {
   Serial.begin(9600);
 
-  while (!Serial)
-    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  //while (!Serial)
+    //delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("SHT31 test");
+  //Serial.println("SHT31 test");
   if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
     Serial.println("Couldn't find SHT31");
     while (1) delay(1);
@@ -28,13 +28,17 @@ void loop() {
   float h = sht31.readHumidity();
 
   if (! isnan(t)) {  // check if 'is not a number'
-    Serial.print("Temp *C = "); Serial.println(t);
+    //Serial.print("Temp *C = "); 
+    Serial.print(t);
   } else { 
     Serial.println("Failed to read temperature");
   }
+
+  Serial.print(',');
   
   if (! isnan(h)) {  // check if 'is not a number'
-    Serial.print("Hum. % = "); Serial.println(h);
+    //Serial.print("Hum. % = "); 
+    Serial.print(h);
   } else { 
     Serial.println("Failed to read humidity");
   }
